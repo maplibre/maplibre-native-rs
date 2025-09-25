64bca0985b283d857f4aef31a9ad0075a3982be8
Frank Elsinga <frank@elsinga.de>
2025-09-25 13:59:02 +0200

    more tests

--- tests/pool.rs
+++ tests/pool.rs
@@ -2,7 +2,7 @@
 
 #![cfg(feature = "pool")]
 
-use insta::assert_debug_snapshot;
+use insta::{assert_binary_snapshot, assert_debug_snapshot};
 use maplibre_native::pool::{PoolError, SingleThreadedRenderPool};
 use std::path::PathBuf;
 
@@ -11,43 +11,6 @@ fn fixture_path(name: &str) -> PathBuf {
         .join("tests")
         .join("fixtures")
         .join(name)
-}
-
-#[test]
-fn global_pool_is_singleton() {
-    let pool1 = SingleThreadedRenderPool::global_pool();
-    let pool2 = SingleThreadedRenderPool::global_pool();
-    assert_eq!(pool1 as *const _, pool2 as *const _);
-}
-
-#[tokio::test]
-async fn invalid_style_path_fails() {
-    let pool = SingleThreadedRenderPool::global_pool();
-    let result = pool
-        .render_tile(PathBuf::from("/nonexistent.json"), 0, 0, 0)
-        .await;
-
-    assert!(result.is_err());
-    assert!(matches!(result, Err(PoolError::IOError(_))));
-}
-
-#[tokio::test]
-async fn concurrent_invalid_requests() {
-    let invalid_path = PathBuf::from("missing.json");
-
-    let handles: Vec<_> = (0..3)
-        .map(|i| {
-            let path = invalid_path.clone();
-            tokio::spawn(async move {
-                let pool = SingleThreadedRenderPool::global_pool();
-                pool.render_tile(path, 0, i, 0).await.is_err()
-            })
-        })
-        .collect();
-
-    for handle in handles {
-        assert!(handle.await.unwrap());
-    }
 }
 
 #[tokio::test]
@@ -88,7 +51,7 @@ async fn error_messages_are_consistent() {
 }
 
 #[tokio::test]
-async fn different_path_errors() {
+async fn io_errors() {
     let pool = SingleThreadedRenderPool::global_pool();
 
     let result = pool
@@ -132,27 +95,21 @@ async fn different_path_errors() {
 }
 
 #[tokio::test]
-async fn basic_tile_rendering() {
-    let pool = SingleThreadedRenderPool::global_pool();
-    let style_path = fixture_path("test-style.json");
-
-    let result = pool.render_tile(style_path, 0, 0, 0).await.unwrap();
-    insta::assert_binary_snapshot!(".png", result.as_slice().to_vec());
-}
-
-#[tokio::test]
-async fn style_switching() {
+async fn style_switching_() {
     let pool = SingleThreadedRenderPool::global_pool();
     let style1 = fixture_path("test-style.json");
     let style2 = fixture_path("test-style-alt.json");
 
-    assert!(pool.render_tile(style1.clone(), 1, 0, 0).await.is_ok());
-    assert!(pool.render_tile(style1.clone(), 1, 0, 1).await.is_ok());
-    assert!(pool.render_tile(style2.clone(), 1, 0, 0).await.is_ok());
+    let result = pool.render_tile(style1.clone(), 1, 0, 0).await.unwrap();
+    assert_binary_snapshot!(".png", result.as_slice().to_vec());
+    let result = pool.render_tile(style1.clone(), 1, 0, 1).await.unwrap();
+    assert_binary_snapshot!(".png", result.as_slice().to_vec());
+    let result = pool.render_tile(style2.clone(), 1, 0, 0).await.unwrap();
+    assert_binary_snapshot!(".png", result.as_slice().to_vec());
 }
 
 #[tokio::test(flavor = "multi_thread")]
-async fn concurrent_rendering() {
+async fn concurrent_rendering_does_not_segfault() {
     let style_path = fixture_path("test-style.json");
 
     let handles: Vec<_> = (0..5)
