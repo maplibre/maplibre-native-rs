f4c336284857e09b151a2ac3f97b734e70c2e040
Frank Elsinga <frank@elsinga.de>
2025-10-01 04:54:39 +0200

    Apply suggestion from @CommanderStorm

--- include/rust_log_observer.h
+++ include/rust_log_observer.h
@@ -21,9 +21,7 @@ private:
 };
 
 // Wrapper function for MapLibre's Log::useLogThread
-void Log_useLogThread(bool enable) {
-    mbgl::Log::useLogThread(enable);
-}
+void Log_useLogThread(bool enable);
 
 } // namespace bridge
 } // namespace mln
