d5c396f59c9e69a7a8bec1d62e0876cfc6e385ad
Martin Marmsoler <martin.marmsoler@gmail.com>
2026-03-21 17:43:42 +0100

    implement wgpu

--- examples/slint/build.rs
+++ examples/slint/build.rs
@@ -1,3 +1,6 @@
 fn main() {
     slint_build::compile("main.slint").expect("Slint build failed");
+
+    // Must be in the main binary build.rs
+    println!("cargo:rustc-link-arg=-Wl,-rpath,/home/martin/GIT/maplibre-native/vendor/wgpu-native/target/x86_64-unknown-linux-gnu/release");
 }
