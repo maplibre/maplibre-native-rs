951f96b1d69ef27283269f2085094b599dae463c
Martin Marmsoler <martin.marmsoler@gmail.com>
2026-03-27 20:19:07 +0100

    use nonamalgam as default

--- build.rs
+++ build.rs
@@ -393,7 +393,7 @@ fn build_mln() {
     println!("cargo:rerun-if-env-changed=MLN_PRECOMPILE");
     println!("cargo:rerun-if-env-changed=MLN_CORE_LIBRARY_USE_AMALGAM");
     let amalgam_lib = !env::var("MLN_CORE_LIBRARY_USE_AMALGAM")
-        .unwrap_or("1".to_string())
+        .unwrap_or("0".to_string())
         .eq("0");
     let precompiled = !env::var("MLN_PRECOMPILE")
         .unwrap_or("0".to_string())
