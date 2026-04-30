e8db3bc06897279d6eb8e39615c2ce2295470628
Martin Marmsoler <martin.marmsoler@gmail.com>
2026-04-30 10:14:26 +0200

    update limits

--- examples/slint/src/main.rs
+++ examples/slint/src/main.rs
@@ -6,7 +6,11 @@ fn main() {
     env_logger::init();
 
     let mut wgpu_settings = slint::wgpu::WGPUSettings::default();
-    wgpu_settings.device_required_limits.max_storage_buffers_per_shader_stage = 1;
+    // MapLibre's shaders use multiple storage buffers in the vertex stage.
+    wgpu_settings.device_required_limits.max_storage_buffers_per_shader_stage = 8;
+    // Some drivers/backends end up with 0 here in downlevel defaults, which fails bind group validation.
+    wgpu_settings.device_required_limits.max_uniform_buffer_binding_size = 4096;
+    wgpu_settings.device_required_limits.max_storage_buffer_binding_size = 4096;
 
     slint::BackendSelector::new()
         .require_wgpu_29(slint::wgpu::WGPUConfiguration::Automatic(wgpu_settings))
