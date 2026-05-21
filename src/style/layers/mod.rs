49ac68544dff3196bea7c118e103f86b10f39de5
Taku Fukada <naninunenor@gmail.com>
2026-05-21 11:30:50 +0900

    split traits module into layers and sources modules

--- src/style/layers/mod.rs
+++ src/style/layers/mod.rs
@@ -3,9 +3,11 @@ mod fill;
 mod id;
 mod line;
 mod symbol;
+mod traits;
 
 pub use circle::CircleLayer;
 pub use fill::FillLayer;
 pub use id::LayerId;
 pub use line::{LineCap, LineJoin, LineLayer};
 pub use symbol::{SymbolAnchor, SymbolLayer};
+pub use traits::Layer;
--- src/style/mod.rs
+++ src/style/mod.rs
@@ -7,15 +7,13 @@ mod image;
 mod layers;
 mod map_style;
 mod sources;
-mod traits;
 
 pub use color::Color;
 pub use error::StyleError;
 pub use geojson::{GeoJson, GeoJsonError};
 pub use image::ImageId;
 pub use layers::{
-    CircleLayer, FillLayer, LayerId, LineCap, LineJoin, LineLayer, SymbolAnchor, SymbolLayer,
+    CircleLayer, FillLayer, Layer, LayerId, LineCap, LineJoin, LineLayer, SymbolAnchor, SymbolLayer,
 };
 pub use map_style::Style;
-pub use sources::{GeoJsonSource, SourceId};
-pub use traits::{Layer, Source};
+pub use sources::{GeoJsonSource, Source, SourceId};
